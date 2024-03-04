2df43f9b912f63d967b3e3e5742abb54af9358d6
cheetahsweater <aikmanfamily@gmail.com>
2024-03-04 12:03:08 -0600

    Reverted testing logic

--- xylq.py
+++ xylq.py
@@ -14,9 +14,9 @@ import asyncio
 
 status = "Cookie Run: Ovenbreak"
 #status = "Testing new features!"
-versionnum = "3.6"
-updatetime = "2024/03/04 11:59"
-changes = "**(3.6) Added rudimentary helper function to ensure easier rolls"
+versionnum = "3.6a"
+updatetime = "2024/03/04 12:03"
+changes = "**(3.6) Added rudimentary helper function to ensure easier rolls\n(a) Reverted testing logic (whoops)"
 path = os.getcwd()
 print(f"XyL-Q v{versionnum}")
 print(updatetime)
@@ -318,7 +318,7 @@ async def on_message(message):
             return
         if message.author.id in bots: #Don't index certain bots (probably going to revise this because it's kind of janky)
             return
-        if message.author.id != 432610292342587392: #Mudae notification logic
+        if message.author.id == 432610292342587392: #Mudae notification logic
             if len(message.embeds) == 1:
                 sourcelines = message.embeds[0].description.split("\n")[:-1]
                 source = ""
